f183201c8675ffbd70bc7d898ad802fec3098a73
exonerated <jordansmithofficially@gmail.com>
2022-11-20 17:09:31 -0500

    fix some things

--- index.js
+++ index.js
@@ -1,84 +1,62 @@
-const inquirer = require('inquirer');
+const inquirer = require("inquirer");
 const fs = require("fs")
-const { renderLicenseBadge, renderLicenseLink, renderLicenseSection } = require('./utils/generateMarkdown');
-
-function readmeTemplate(answers) {
-    return `
-## ${answers.projectName}
-
-## Table of Contents
-
-* [Description](#description)
-
-* [Installation](#installation)
-
-* [Usage](#usage)
-
-* [License](#license)
-
-* [Contributing](#contributing)
-
-* [Test](#test)
-
-* [Questions](#questions)
-
-
-
-${renderLicenseLink(answers.license)}
-
-
-## Description
-    ${answers.description}
-
-## Installation
-    ${answers.installation}
-
-${renderLicenseSection(answers.license)}
-${renderLicenseBadge(answers.license)}
-
-## questions
-Here is a link to my GitHub Profile if you want to see more of my work: [${answers.github}](https://github.com/${answers.github})
-
-`
-}
-
+const generateMarkDown = require("./utils/generateMarkdown")
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: "input",
+        message: "What is your Title",
+        name: "title",
+    },
+    {
+        type: "input",
+        message: "What was your motivation?",
+        name: "Motivation",
+    },
+    {
+        type: "input",
+        message: "Why did you build this project?",
+        name: "Project",
+    },
+    {
+        type: "input",
+        message: "What problem does it solve?",
+        name: "Solve",
+    },
+    {
+        type: "input",
+        message: "What did you learn?",
+        name: "Learn",
+    },
+    {
+        type: "input",
+        message: "What makes your project stand out?",
+        name: "StandOut",
+    },
+    {
+        type: "list",
+        message: "What license does you application use?",
+        name: "license",
+        choices: ['The MIT License', 'GNU General Public License version 3', 'other']
+    },
+    {
+        type: "input",
+        message: "What is your GitHub user name?",
+        name: "github",
+    },
+];
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, generateMarkDown(data), (err) =>
+   err ? console.error(err) : console.log("You have successfully generated your file!")
+    );
+};
+// TODO: Create a function to initialize app
 function init() {
-    inquirer
-        .prompt([
-            {
-                type: 'input',
-                name: 'projectName',
-                message: 'What is your project name?',
-            },
-            {
-                type: 'input',
-                name: 'description',
-                message: 'Give a brief description of your application.'
-            },
-            {
-                type: 'input',
-                name: 'installation',
-                message: 'How can users install your application?'
-            },
-            {
-                type: 'list',
-                name: 'license',
-                message: 'Please select a license',
-                choices: ['MIT', 'Apache', 'BSD', 'None']
-            },
-            {
-                type: 'input',
-                name: 'github',
-                message: 'Please enter your GitHub username'
-            },
-        ])
-        .then(answers => {
-            console.info('Answer:', answers);
-            fs.writeFile('readme.md', readmeTemplate(answers), (err) =>
-                err ? console.log(err) : console.log('Successfully created readme.md!')
-            );
-
-        });
-}
-
+    const startQuestions = () => {
+        return inquirer.prompt(questions);
+    };
+    startQuestions().then((response) => writeToFile("README.md", response))
+};
+// Function call to initialize app
 init();
